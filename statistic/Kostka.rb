27b9efc9ffbfbed2fbe02aee4867c360265a48bc
sdhr <mailbox_1729@hotmail.com>
2017-02-09 23:09:10 +0900

    Changed Kostka to a Module

--- statistic/Kostka.rb
+++ statistic/Kostka.rb
@@ -1,30 +1,33 @@
 require_relative "Statistic"
 require_relative "../base/Polynomial"
 
-def Kostka(shape,weight)
-	temp=[0]
-	Tableau.set(shape,weight).each{|tab|
-		c=Statistic.charge(tab.read.reverse)
-		if temp[c]
-			temp[c]+=1
-		else
-			temp[c]=1
-		end
-	}
-	return temp.to_polynomial
-end
+module Kostka
+	def polynomial(shape,weight)
+		temp=[0]
+		Tableau.set(shape,weight).each{|tab|
+			c=Statistic.charge(tab.read.reverse)
+			if temp[c]
+				temp[c]+=1
+			else
+				temp[c]=1
+			end
+		}
+		return temp.to_polynomial
+	end
+	module_function :polynomial
 
-def modified_Kostka(shape,weight)
-	temp=[0]
-	Tableau.set(shape,weight).each{|tab|
-		c=Statistic.cocharge(tab.read.reverse)
-		if temp[c]
-			temp[c]+=1
-		else
-			temp[c]=1
-		end
-	}
-	return temp.to_polynomial
+	def modified(shape,weight)
+		temp=[0]
+		Tableau.set(shape,weight).each{|tab|
+			c=Statistic.cocharge(tab.read.reverse)
+			if temp[c]
+				temp[c]+=1
+			else
+				temp[c]=1
+			end
+		}
+		return temp.to_polynomial
+	end
+	module_function :modified
 end
-
 
--- statistic/Kostka_test_unit.rb
+++ statistic/Kostka_test_unit.rb
@@ -2,23 +2,23 @@ require_relative "Kostka"
 require "test/unit"
 
 class Tester < Test::Unit::TestCase
-	def test_Kostka
-		assert_equal " 0", Kostka([1,1,1,1,1],[2,1,1,1]).to_string(["t"])
-		assert_equal " 1", Kostka([2,1,1,1],[2,1,1,1]).to_string(["t"])
-		assert_equal " t +t^2", Kostka([2,2,1],[2,1,1,1]).to_string(["t"])
-		assert_equal " t +t^2 +t^3", Kostka([3,1,1],[2,1,1,1]).to_string(["t"])
-		assert_equal " t^2 +t^3 +t^4", Kostka([3,2],[2,1,1,1]).to_string(["t"])
-		assert_equal " t^3 +t^4 +t^5", Kostka([4,1],[2,1,1,1]).to_string(["t"])
-		assert_equal " t^6", Kostka([5],[2,1,1,1]).to_string(["t"])
+	def test_polynomial
+		assert_equal " 0", Kostka.polynomial([1,1,1,1,1],[2,1,1,1]).to_string(["t"])
+		assert_equal " 1", Kostka.polynomial([2,1,1,1],[2,1,1,1]).to_string(["t"])
+		assert_equal " t +t^2", Kostka.polynomial([2,2,1],[2,1,1,1]).to_string(["t"])
+		assert_equal " t +t^2 +t^3", Kostka.polynomial([3,1,1],[2,1,1,1]).to_string(["t"])
+		assert_equal " t^2 +t^3 +t^4", Kostka.polynomial([3,2],[2,1,1,1]).to_string(["t"])
+		assert_equal " t^3 +t^4 +t^5", Kostka.polynomial([4,1],[2,1,1,1]).to_string(["t"])
+		assert_equal " t^6", Kostka.polynomial([5],[2,1,1,1]).to_string(["t"])
 	end
-	def test_modified_Kostka
-		assert_equal " 0", modified_Kostka([1,1,1,1,1],[2,1,1,1]).to_string(["t"])
-		assert_equal " t^6", modified_Kostka([2,1,1,1],[2,1,1,1]).to_string(["t"])
-		assert_equal " t^4 +t^5", modified_Kostka([2,2,1],[2,1,1,1]).to_string(["t"])
-		assert_equal " t^3 +t^4 +t^5", modified_Kostka([3,1,1],[2,1,1,1]).to_string(["t"])
-		assert_equal " t^2 +t^3 +t^4", modified_Kostka([3,2],[2,1,1,1]).to_string(["t"])
-		assert_equal " t +t^2 +t^3", modified_Kostka([4,1],[2,1,1,1]).to_string(["t"])
-		assert_equal " 1", modified_Kostka([5],[2,1,1,1]).to_string(["t"])
+	def test_modified
+		assert_equal " 0", Kostka.modified([1,1,1,1,1],[2,1,1,1]).to_string(["t"])
+		assert_equal " t^6", Kostka.modified([2,1,1,1],[2,1,1,1]).to_string(["t"])
+		assert_equal " t^4 +t^5", Kostka.modified([2,2,1],[2,1,1,1]).to_string(["t"])
+		assert_equal " t^3 +t^4 +t^5", Kostka.modified([3,1,1],[2,1,1,1]).to_string(["t"])
+		assert_equal " t^2 +t^3 +t^4", Kostka.modified([3,2],[2,1,1,1]).to_string(["t"])
+		assert_equal " t +t^2 +t^3", Kostka.modified([4,1],[2,1,1,1]).to_string(["t"])
+		assert_equal " 1", Kostka.modified([5],[2,1,1,1]).to_string(["t"])
 	end
 end
 
